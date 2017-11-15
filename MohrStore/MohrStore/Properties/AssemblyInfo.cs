83428ecff349d9a81ff1fe096f81218c5ac639a7
Dan Mohr <dmohr@utilant.com>
2017-11-15 14:46:24 -0500

    rebuilding as n-tier

--- MohrStore/MohrStore/Properties/AssemblyInfo.cs
+++ MohrStore/MohrStore/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52fdd7fc-e29e-4e45-97cf-9fd04c5241c5")]
+[assembly: Guid("e7a74801-5eb2-4c16-a7de-b00f7ac752bb")]
 
 // Version information for an assembly consists of the following four values:
 //
