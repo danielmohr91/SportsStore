44bf2c47eec3816910d393071c896649bbe5e276
Dan Mohr <dmohr@utilant.com>
2017-11-15 14:50:22 -0500

    fleshed out tiers

---
+++ MohrStore/MohrStore.BusinessLogic/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohrStore.BusinessLogic
+{
+    public class Class1
+    {
+        // https://www.codeproject.com/Articles/70061/Architecture-Guide-ASP-NET-MVC-Framework-N-tier-En
+    }
+}
